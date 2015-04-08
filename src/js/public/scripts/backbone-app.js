6170833b5e53d4d9eb9ee85ffa601e4e537164cb
Vyacheslav Goldenshteyn <vyacheslav.goldenshteyn@gmail.com>
2015-04-08 07:26:01 +0300

    DEGREE-34: Верстка пользовательского интерфейса  для получения списка предметов определенной специальности
    * полностью переписан почти с нуля весь фронтэнд
    * вся необходимая функциональноть вроде бы работает

--- src/js/public/scripts/backbone-app.js
+++ src/js/public/scripts/backbone-app.js
@@ -1,12 +1,224 @@
-var app = {
-    Collections: {}
+var Features = {
+    Dashboard: "Features::Dashboard",
+    DegreeSummary: "Features::DegreeSummary",
+    CourseSearch: "Features::CourseSearch",
+    TopicSearch: "Features::TopicSearch"
 };
-// we need to define model before the collection
+
+var SideBarStatus = {
+    Hidden: "SideBarStatus::Hidden",
+    Shown: "SideBarStatus::Shown"
+};
+
+// App
+
+var App = Backbone.Model.extend({
+    defaults: {
+        activeFeature: Features.Dashboard,
+        sideBarStatus: SideBarStatus.Shown,
+        router: null
+    },
+    initialize: function () {
+        var self = this;
+
+        self.router = new (Backbone.Router.extend({
+            routes: {
+                '': 'index',
+                'degree-summary': function () {
+                    self.set({activeFeature: Features.DegreeSummary});
+                },
+                'course-search': function () {
+                    self.set({activeFeature: Features.CourseSearch});
+                },
+                'topic-search': function () {
+                    self.set({activeFeature: Features.TopicSearch});
+                }
+            },
+            initialize: function () {
+                Backbone.history.start();
+            }
+        }))
+
+    }
+});
+
+// SideBar
+
+var SidebarModel = Backbone.Model.extend({
+    defaults: {
+        app: null
+    }
+});
+
+var SidebarView = Backbone.View.extend({
+    template: _.template($('#sidebar').html()),
+    initialize: function () {
+        var render = _.bind(this.render, this);
+
+        this.model.on('change:app', function (event) {
+            render();
+
+            event.changed.app.on('all', render);
+        });
+    },
+    render: function () {
+        this.$el.html(this.template({
+            sidebar: {
+                activeFeature: this.model
+                    .get('app')
+                    .get('activeFeature')
+            }
+        }));
+
+        return this.$el;
+    }
+});
+
+// Content
+var ContentModel = Backbone.Model.extend({
+    defaults: {
+        app: null,
+        featureViews: {},
+        activeView: null
+    }
+});
+
+var ContentView = Backbone.View.extend({
+    initialize: function () {
+        var updateActiveView = _.bind(function () {
+            return this.model.set({
+                activeView: this.model.get('featureViews')[this.model
+                    .get('app')
+                    .get('activeFeature')]
+            });
+        }, this);
+        var render = _.bind(this.render, this);
+
+        this.model.on('change:app', function (event) {
+            updateActiveView();
+
+            event.changed.app.on('change:activeFeature', updateActiveView);
+        });
+
+        this.model.on('change:activeView', render);
+    },
+    render: function () {
+        var activeView = this.model.get('activeView');
+
+        return activeView
+            ? activeView.render()
+            : null;
+    }
+});
+
+// Search
+
+var SearchModel = Backbone.Model.extend({
+    defaults: {
+        inputId: '',
+        buttonId: '',
+        placeholder: '',
+        resultId: '',
+        onSearch: function (query) {
+        },
+        resultView: null
+    }
+});
+
+var SearchView = Backbone.View.extend({
+    template: _.template($('#search-view').html()),
+    render: function () {
+        var element = this.$el
+        var inputId = this.model.get('inputId');
+        var placeholder = this.model.get('placeholder');
+        var buttonId = this.model.get('buttonId');
+        var resultId = this.model.get('resultId');
+        var onSearch = this.model.get('onSearch');
+        var resultView = this.model.get('resultView');
+        var searchVal = $('#' + inputId, this.$el).val();
+
+        element.html(this.template({
+            search: {
+                inputId: inputId,
+                placeholder: placeholder,
+                buttonId: buttonId,
+                resultId: resultId
+            }
+        }));
+
+        $('#' + resultId, element).append(resultView.render())
+
+        var search = function () {
+            var searchVal = $('#' + inputId, element).val();
+
+            onSearch(searchVal);
+        };
+
+        $('#' + inputId, element).val(searchVal);
+        $('#' + buttonId, element).click(search);
+        $('#' + inputId, element).keydown(function (e) {
+            if (e.keyCode == 13) {
+                search();
+            }
+        });
+
+        return element;
+    }
+});
+
+
+// Courses
+
 var CourseModel = Backbone.Model.extend({
     initialize: function () {
         this.view = new CourseView({model: this});
     }
 });
+
+var CourseView = Backbone.View.extend({
+    template: _.template($('#course-view').html()),
+    initialize: function () {
+        this.setElement(this.template({course: this.model.toJSON()}));
+    }
+});
+
+var CoursesCollection = Backbone.Collection.extend({
+    url: function () {
+        return _.template('/v1/courses?search=<%= searchQuery %>', {searchQuery: this.searchQuery});
+    },
+    model: CourseModel, //define class of models in collection
+    initialize: function () {
+        // by default show all courses
+        this.searchQuery = '';
+    }
+});
+
+var CoursesListView = Backbone.View.extend({
+    initialize: function () {
+        this.$list = $('<div></div>');
+    },
+
+    render: function () {
+        var self = this;
+        this.$list.html('');
+
+        if (this.collection.models.length == 0) {
+            this.notFound();
+        } else {
+            _.each(this.collection.models, function (model) {
+                self.$list.append(model.view.$el);
+            });
+        }
+
+        return this.$list;
+    },
+
+    notFound: function () {
+        this.$list.html('<h2>No such courses ;(</h2>');
+    }
+});
+
+// Topics
 
 var TopicModel = Backbone.Model.extend({
     initialize: function () {
@@ -14,117 +226,33 @@ var TopicModel = Backbone.Model.extend({
     }
 });
 
-var CourseView = Backbone.View.extend({
-    template: _.template($('#course_view').html()),
-    initialize: function () {
-        this.setElement(this.template({course: this.model.toJSON()}));
-    }
-});
-
 var TopicView = Backbone.View.extend({
-    template: _.template($('#topic_view').html()),
+    template: _.template($('#topic-view').html()),
     initialize: function () {
         this.setElement(this.template({topic: this.model.toJSON()}));
     }
 });
 
-var CoursesCollection = Backbone.Collection.extend({
+var TopicsCollection = Backbone.Collection.extend({
     url: function () {
-        return _.template('/v1/courses?search=<%= searchStr %>', {searchStr: this.searchStr});
+        return _.template('/v1/topics?search=<%= searchQuery %>', {searchQuery: this.searchQuery});
     },
     model: CourseModel, //define class of models in collection
     initialize: function () {
-
         // by default show all courses
-        this.searchStr = '';
-
-        // set view of collection
-        this.view = new CoursesListView({collection: this});
-
-        // bind render of model on reset collection
-        this.on('reset', this.view.render, this.view);
-
-        // fetch data from url with reset
-        this.fetch({reset: true});
+        this.searchQuery = '';
     }
 });
 
-
-var TopicsCollection = Backbone.Collection.extend({
-    url: function () {
-        return _.template('/v1/topics/?search=<%= searchStr %>', {searchStr: this.searchStr});
-    },
-    model: TopicModel, //define class of models in collection
-    initialize: function () {
-
-        // by default show all courses
-        this.searchStr = '';
-
-        // set view of collection
-        this.view = new TopicsListView({collection: this});
-
-        // bind render of model on reset collection
-        this.on('reset', this.view.render, this.view);
-
-        // fetch data from url with reset
-        this.fetch({reset: true});
-    }
-});
-
-
-// view of collection
-var CoursesListView = Backbone.View.extend({
-    el: $('#content'), // DOM-element of widget'а
-
-    events: {
-        'click #search_courses_btn': 'searchCourses',
-        'keydown #search_courses_input': 'searchCourses'
-    },
-
-    initialize: function () {
-        this.$list = $('#list', this.$el); // set $list where will be rendered search results (inside $el)
-    },
-
-    render: function () {
-        var self = this;
-        this.$list.html('');
-        if (this.collection.models.length == 0) {
-            this.notFound();
-        } else {
-            _.each(this.collection.models, function (model) {
-                self.$list.append(model.view.$el);
-            });
-        }
-    },
-
-    notFound: function () {
-        this.$list.html('<h2>Нет результатов</h2>');
-    },
-
-    searchCourses: function (e) {
-        if ((e.type === 'keydown' && e.keyCode === 13) || e.type === 'click') {
-            var searchVal = $('#search_courses_input', this.$el).val();
-
-            this.collection.searchStr = searchVal;
-            this.collection.fetch({reset: true});
-        }
-    }
-});
 var TopicsListView = Backbone.View.extend({
-    el: $('#content'), // DOM-element of widget'а
-
-    events: {
-        'click #search_courses_btn': 'searchCourses',
-        'keydown #search_courses_input': 'searchCourses'
-    },
-
     initialize: function () {
-        this.$list = $('#list', this.$el); // set $list where will be rendered search results (inside $el)
+        this.$list = $('<div></div>')
     },
 
     render: function () {
         var self = this;
         this.$list.html('');
+
         if (this.collection.models.length == 0) {
             this.notFound();
         } else {
@@ -132,68 +260,128 @@ var TopicsListView = Backbone.View.extend({
                 self.$list.append(model.view.$el);
             });
         }
+
+        return this.$list;
     },
 
     notFound: function () {
-        this.$list.html('<h2>Нет результатов</h2>');
-    },
-
-    searchCourses: function (e) {
-        if ((e.type === 'keydown' && e.keyCode === 13) || e.type === 'click') {
-            var searchVal = $('#search_courses_input', this.$el).val();
-
-            this.collection.searchStr = searchVal;
-            this.collection.fetch({reset: true});
-        }
+        this.$list.html('<h2>No such topics ;(</h2>');
     }
 });
 
-var Router = Backbone.Router.extend({
+// Degree
 
-    routes: {
-        '': 'index',
-        'degree_summary': 'degree_summary',
-        'course_search': 'course_search',
-        'topic_search': 'topic_search'
-    },
-
+var DegreeModel = Backbone.Model.extend({
     initialize: function () {
-        Backbone.history.start();
-    },
-
-    index: function () {
-        // костыль пока (пока нет view у sidebar)
-        //$('ul.sidebar-nav li a#a_f1').addClass('bg-primary').parent().siblings().find('a.bg-primary').removeClass('bg-primary');
-        //if (app.Collections.courses_collection) {
-        //    app.Collections.courses_collection.view.notFound(); // заглушка пока
-        //}
-    },
-
-    degree_summary: function () {
-        $('ul.sidebar-nav li a#a_ds').addClass('bg-primary').parent().siblings().find('a.bg-primary').removeClass('bg-primary');
-        if (app.Collections.courses_collection) {
-            app.Collections.courses_collection.view.notFound();
-        }
-    },
-
-    course_search: function () {
-        $('ul.sidebar-nav li a#a_cs').addClass('bg-primary').parent().siblings().find('a.bg-primary').removeClass('bg-primary');
-        if (app.Collections.courses_collection) {
-            app.Collections.courses_collection.fetch({reset: true});
-        } else {
-            app.Collections['courses_collection'] = new CoursesCollection();
-        }
-    },
-
-    topic_search: function () {
-        $('ul.sidebar-nav li a#a_ts').addClass('bg-primary').parent().siblings().find('a.bg-primary').removeClass('bg-primary');
-        if (app.Collections.topics_collection) {
-            app.Collections.topics_collection.fetch({reset: true});
-        } else {
-            app.Collections['topics_collection'] = new TopicsCollection();
-        }
+        this.view = new DegreeView({model: this});
     }
 });
 
-app.router = new Router();
+var DegreeView = Backbone.View.extend({
+    template: _.template($('#degree-view').html()),
+    initialize: function () {
+        this.setElement(this.template({degree: this.model.toJSON()}));
+    }
+});
+
+var DegreesCollection = Backbone.Collection.extend({
+    url: function () {
+        return '/v1/degrees';
+    },
+    model: DegreeModel
+});
+
+var DegreesListView = Backbone.View.extend({
+    render: function () {
+        var list = $('<div id="content">');
+
+        _.each(this.collection.models, function (model) {
+            list.append(model.view.$el);
+        });
+
+        this.$el.html(list.html());
+        return this.$el;
+    }
+});
+
+// main
+
+var app = new App();
+app.on('change:activeFeature', function (event) {
+    console.log(event.changed.activeFeature)
+});
+
+var sidebarModel = new SidebarModel();
+var sidebarView = new SidebarView({model: sidebarModel, el: $('#sidebar-wrapper')});
+
+//
+var coursesCollection = new CoursesCollection();
+var coursesView = new CoursesListView({collection: coursesCollection});
+
+var coursesSearchModel = new SearchModel({
+    inputId: 'coursesSearch-input',
+    buttonId: 'coursesSearch-button',
+    resultId: 'coursesSearch-result',
+    placeholder: 'Course name',
+    resultView: coursesView,
+    onSearch: function (query) {
+        coursesCollection.searchQuery = query;
+        coursesCollection.fetch({reset: true});
+    }
+});
+var coursesSearchView = new SearchView({model: coursesSearchModel, el: $('#content')});
+coursesCollection.on('reset', coursesSearchView.render, coursesSearchView);
+
+//
+var topicsCollection = new TopicsCollection();
+var topicsView = new TopicsListView({collection: topicsCollection});
+
+var topicsSearchModel = new SearchModel({
+    inputId: 'topicsSearch-input',
+    buttonId: 'topicsSearch-button',
+    resultId: 'topicsSearch-result',
+    placeholder: 'Topic name',
+    resultView: topicsView,
+    onSearch: function (query) {
+        topicsCollection.searchQuery = query;
+        topicsCollection.fetch({reset: true});
+    }
+});
+var topicsSearchView = new SearchView({model: topicsSearchModel, el: $('#content')});
+topicsCollection.on('reset', topicsSearchView.render, topicsSearchView);
+
+//
+var degreesCollection = new DegreesCollection();
+var degreesView = new DegreesListView({collection: degreesCollection, el: $('#content')});
+degreesCollection.on('reset', degreesView.render, degreesView);
+
+var featureViews = {};
+featureViews[Features.DegreeSummary] = degreesView;
+featureViews[Features.CourseSearch] = coursesSearchView;
+featureViews[Features.TopicSearch] = topicsSearchView;
+
+var prefetches = {};
+prefetches[Features.DegreeSummary] = function () {
+    degreesCollection.fetch({reset: true})
+};
+prefetches[Features.CourseSearch] = function () {
+    coursesCollection.fetch({reset: true})
+};
+prefetches[Features.TopicSearch] = function () {
+    topicsCollection.fetch({reset: true})
+};
+
+var contentModel = new ContentModel({featureViews: featureViews});
+var contentView = new ContentView({model: contentModel});
+
+app.on('change:activeFeature', function (event) {
+    var prefetch = prefetches[event.changed.activeFeature];
+
+    if (prefetch)
+        prefetch();
+});
+
+
+sidebarModel.set({app: app});
+contentModel.set({app: app});
 
