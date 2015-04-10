b92cd34da096e463580802eb3c3c4f0a670a30a5
Vyacheslav Goldenshteyn <vgoldenshteyn@productengine.com>
2015-04-10 17:00:16 +0300

    DEGREE-38: Прикрутить Require.js DEGREE-37: Прикрутить React.js DEGREE-42: Попробовать прикрутить fn.js
    * скелет для require.js
    * скелет для react.js
    * скелет для rambda.js

---
+++ client/src/js/app.js
@@ -0,0 +1,11 @@
+(require).config({
+    paths: {
+        'react': 'http://fb.me/react-0.13.1',
+        'hogan': 'http://twitter.github.com/hogan.js/builds/3.0.1/hogan-3.0.1',
+        'ramda': 'http://cdn.jsdelivr.net/ramda/0.13/ramda.min'
+    }
+});
+
+define(['react', 'hogan', 'ramda'], function(React, Hogan, R) {
+
+});
